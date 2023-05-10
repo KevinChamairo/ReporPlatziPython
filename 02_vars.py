fcb3e12790547954bdcc2a2f36eacad6c340a840
KevinChamairo <kevin.chamairoriveros@gmail.com>
2023-05-10 17:52:39 -0500

    commit de Python

---
+++ 02_vars.py
@@ -0,0 +1,26 @@
+#VIDEO 3
+print("Hola, esto es python xd")
+#VIDEO 4
+print(2+2)
+print(2*8)
+
+"""
+Estos es para full comentarios
+demasiados comentarios
+"""
+#VIDEO 6
+#Esto es una variable 
+my_name = "kevin"
+print("Hola mi nombre es "+my_name)
+
+#Esto es una variable con un numero
+my_age = 24
+print("Y tengo "+str(my_age)+" años")
+#Cambio de nombre
+my_name = "Arnold"
+print("Aqui se cambia el nombre", my_name)
+
+#Input, ingreso de valores
+my_name2 = input("¿Cual es tu nombre?")
+print("Tu nombre es: ", my_name2)
+
